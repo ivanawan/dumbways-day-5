78a16b6aae0093ffc4bbc011fccdc4b5c62c2480
ivansetiawan <ivansetiawanvll@gmail.com>
2022-05-17 20:01:48 +0700

    done

--- asset/index.js
+++ asset/index.js
@@ -1,3 +1,6 @@
+const month=["Januari","Febuary","Maret","April","Mei","Juni","Juli","Agustus","September","Oktober","November","Desember"];
+
+
 
 function submit(){
     let {name,email,number,subject,message}="";
@@ -22,46 +25,77 @@ function submit(){
     mail.click();
 }
 
-let dataProject=[];
+
+
+let listProject=[];
+
+if(localStorage.getItem('listProject') != null){
+    listProject=JSON.parse(localStorage.getItem('listProject'));
+}
+
 function addproject(){
+ var textinputs = document.querySelectorAll('input[type=checkbox]:checked'); 
+ let checkbox=[];
 
-  console.log(document.getElementsByName("techno"));
+ let dateStart=new Date(document.getElementById("date-start").value);
+ let dateEnd=new Date(document.getElementById("date-end").value);
+ let duration =difference(dateStart, dateEnd);
+ let date = `${dateStart.getDate()} ${month[dateStart.getMonth()]} ${dateStart.getFullYear()} - ${dateEnd.getDate()} ${month[dateEnd.getMonth()]} ${dateEnd.getFullYear()}`;
+//  console.log(date);
 
-    dataProject.push({
+  textinputs.forEach(el => {
+      checkbox.push(el.value);
+  });
+
+    listProject.push({
         title:document.getElementById("title").value,
         image:URL.createObjectURL(document.getElementById("image").files[0]),
         autor:"ivan setiawan",
-        date:new Date(),
+        duration:duration<30 ? duration+" hari" : parseInt(duration/30)+" bulan",
+        date: date,
+        checkbox: checkbox,
         content:document.getElementById("content").value
     });
 
-    // console.log(dataProject);
+    localStorage.setItem('listProject', JSON.stringify(listProject));
     renderPage();
 }
 
 function renderPage(){
-    let blogContainer = document.getElementById('grid');
+    // console.log(listProject);
 
-    blogContainer.innerHTML = '';
-
-    dataProject.forEach((data,index)=>{
-        blogContainer.innerHTML+=`<a href="detailproject.html" class="card-grid">
+    let scriptHtml='';
+    listProject.forEach((data,index)=>{
+        scriptHtml +=`<a href="detailproject.html" onclick="getId(${index})" class="card-grid">
         <img src="${data.image}" alt="">
         <p class="title">${data.title}</p>
-        <p class="durasi">Durasi : 3 bulan</p>
-        <p class="deskripsi">${data.content}</p>
+        <p class="durasi">Durasi : ${data.duration}</p>
+        <p class="deskripsi">${data.content.substring(0, 20)}</p>
         <div class="tech">
-            <i class="ri-attachment-2"></i>
-            <i class="ri-attachment-2"></i>
-            <i class="ri-attachment-2"></i>
+            ${data.checkbox.toString().replaceAll(',',' ')}
         </div>
         <div style=" display: flex;">
         <button onclick="edit(${index})" class="butom-action" style="margin-right: 10px;">Edit</button>
         <button onclick="hapus(${index})" class="butom-action">Hapus</button>
         </div>
     </a>`
-    })
-  
+    });
+    // console.log(scriptHtml);
+    document.getElementById('grid').innerHTML=scriptHtml;
+}
+
+function getId(id){
+    localStorage.setItem("id",id);
+}
+
+function renderPageDetail(){
+ const item =listProject[localStorage.getItem("id")];
+ document.getElementById('title').innerHTML =item.title;
+//  document.getElementById('image').src =item.image;
+document.getElementById('date').innerHTML=item.date;
+document.getElementById('duration').innerHTML=item.duration;
+document.getElementById('content').innerHTML=item.content;
+document.getElementById('teknologi').innerHTML=item.checkbox.toString().replaceAll(',',' ');
 }
 
 function edit(index){
@@ -71,3 +105,14 @@ console.log(index);
 function hapus(index){
     console.log(index);
 }
+
+
+
+
+function difference(date1, date2) {  
+    const date1utc = Date.UTC(date1.getFullYear(), date1.getMonth(), date1.getDate());
+    const date2utc = Date.UTC(date2.getFullYear(), date2.getMonth(), date2.getDate());
+      day = 1000*60*60*24;
+    return(date2utc - date1utc)/day
+}
+
